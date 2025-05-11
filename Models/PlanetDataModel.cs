130039f194f313c263485946fa0b530a0f84f086
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2025-05-11 23:05:28 +0200

    add styling to planets

--- Models/PlanetDataModel.cs
+++ Models/PlanetDataModel.cs
@@ -1,14 +1,17 @@
-public class PlanetDataModel
+namespace Easyweb.Models
 {
-    public string Title { get; set; }
-    public ThumbnailData Thumbnail { get; set; }
-    public string Description { get; set; }
-    public string Extract { get; set; }
-}
+    public class PlanetDataModel
+    {
+        public string Title { get; set; }
+        public ThumbnailData Thumbnail { get; set; }
+        public string Description { get; set; }
+        public string Extract { get; set; }
+    }
 
-public class ThumbnailData
-{
-    public string Source { get; set; }
-    public int Width { get; set; }
-    public int Height { get; set; }
+    public class ThumbnailData
+    {
+        public string Source { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
 }
