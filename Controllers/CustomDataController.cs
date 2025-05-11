8d42a91db905c13d14ed8fdb16edef1827595098
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2025-05-11 23:04:27 +0200

    add data to match planet model

--- Controllers/CustomDataController.cs
+++ Controllers/CustomDataController.cs
@@ -3,17 +3,31 @@ using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Easyweb.Controllers
 {
+    using Easyweb.Models;
     public class CustomDataController : Controller
     {
+
+
         // Action to get the planet list and pass it to the view
         [HttpGet("/planets")]
         public IActionResult Index()
         {
-            var planets = new[] { "Mercury_(planet)", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
-
+            var planets = new List<Planet>
+            {
+                new Planet { Name = "Sun", WikiName = "Sun", SizeFactor = 150, Color = "#FFD562", HasRings = false },
+                new Planet { Name = "Mercury", WikiName = "Mercury_(planet)", SizeFactor = 30, Color = "#aaa", HasRings = false },
+                new Planet { Name = "Venus", WikiName = "Venus", SizeFactor = 50, Color = "#c9b37e", HasRings = false },
+                new Planet { Name = "Earth", WikiName = "Earth", SizeFactor = 55, Color = "#2e77bc", HasRings = false },
+                new Planet { Name = "Mars", WikiName = "Mars", SizeFactor = 40, Color = "#d14b3e", HasRings = false },
+                new Planet { Name = "Jupiter", WikiName = "Jupiter", SizeFactor = 80, Color = "#d6a16d", HasRings = false },
+                new Planet { Name = "Saturn", WikiName = "Saturn", SizeFactor = 70, Color = "#ecdcbf", HasRings = true },
+                new Planet { Name = "Uranus", WikiName = "Uranus", SizeFactor = 50, Color = "#7fdbff", HasRings = true },
+                new Planet { Name = "Neptune", WikiName = "Neptune", SizeFactor = 50, Color = "#4169e1", HasRings = true }
+            };
             return View(planets);
         }
 
